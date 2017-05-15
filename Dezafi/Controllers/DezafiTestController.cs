6d28e3e2d5a7ec98917fed50866099cfb193e615
ray <raymondloiseau@gmail.com>
2017-05-14 21:38:01 -0400

    Update the project one more time

---
+++ Dezafi/Controllers/DezafiTestController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Dezafi.Models;
+
+namespace Dezafi.Controllers
+{
+    public class DezafiTestController : ApiController
+    {
+        List<WinningNumbersModel> WinnningNumberSet = new List<WinningNumbersModel>() {
+           new WinningNumbersModel { CurrentDate = "05/05/2017", WinningNumbers = "3 56 43 8 23 7" },
+           new WinningNumbersModel { CurrentDate = "05/06/2017", WinningNumbers = "3 44 22 8 10 4" }
+       };
+
+        public IEnumerable<WinningNumbersModel> GetWinningNumbers() {
+            return WinnningNumberSet;
+        }
+    }
+}
---
+++ Dezafi/Models/WinningNumbersModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+namespace Dezafi.Models
+{
+    public class WinningNumbersModel
+    {
+        [System.ComponentModel.DataAnnotations.Required]
+        [DataType(DataType.DateTime)]
+        [Display(Name = "Current date")]
+        public string CurrentDate { get; set; }
+
+        [Required]
+        [StringLength(20, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 10)]
+        [DataType(DataType.Text)]
+        [Display(Name = "Winning Numbers")]
+        public string WinningNumbers { get; set; }
+        
+    }
+
+    
+}
